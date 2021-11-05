754d10fbe9759794d102fdc90d5bba3007a9a8a8
Adebayo Ilerioluwa <adebayorilerioluwa@gmail.com>
2021-11-06 00:05:31 +0100

    chore: offline crypto pay util

---
+++ packages/offline-pay-with-crypto-util/README.md
@@ -0,0 +1,11 @@
+# `offline-pay-with-crypto-util`
+
+> TODO: description
+
+## Usage
+
+```
+const offlinePayWithCryptoUtil = require('offline-pay-with-crypto-util');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/offline-pay-with-crypto-util/__tests__/offline-pay-with-crypto-util.test.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const offlinePayWithCryptoUtil = require('../lib/offline-pay-with-crypto-util')
+
+describe('offline-pay-with-crypto-util', () => {
+  it('needs tests`', () => {
+    expect(1).toBe(1)
+  })
+})
