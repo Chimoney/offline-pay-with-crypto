c3303e4455acc11de2caa1e80b5a9830605f3cc4
lawal mukhtar <lmukhtar29@gmail.com>
2021-11-14 20:28:50 -0800

    added landing and documentation page

---
+++ packages/offline-pay-with-crypto-landing-page/src/components/HomepageFeatures.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './HomepageFeatures.module.css';
+
+const FeatureList = [
+  {
+    title: 'Easy to Use',
+    Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
+    description: (
+      <>
+        Offline Pay with crypto was designed from the ground up to be easily installed and
+        used experience amazing crypto payment integration.
+      </>
+    ),
+  },
+  {
+    title: 'Focus on What Matters',
+    Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
+    description: (
+      <>
+        Offline Pay with crypto remove the burden of Payment Integration, as we do the work for you.
+      </>
+    ),
+  },
+  {
+    title: 'Powered by Chimoney',
+    Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
+    description: (
+      <>
+        Send and receive Gift Cards, Crypto, Airtime and Bank transfers to million people globally
+      </>
+    ),
+  },
+];
+
+function Feature({Svg, title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} alt={title} />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
