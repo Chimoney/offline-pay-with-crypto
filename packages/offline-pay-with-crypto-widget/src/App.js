f5e899a5feaa59b45f64ec245cfe6a64599697ce
Uchi Uchibeke <uchi.uchibeke@gmail.com>
2021-11-21 18:30:09 -0500

    Resolve ui issues

--- packages/offline-pay-with-crypto-widget/src/App.js
+++ packages/offline-pay-with-crypto-widget/src/App.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import './App.css';
-import { Modal } from './components';
-
+import React from 'react'
+import './App.css'
+import { Modal } from './components'
 
 function App() {
-  const config ={}
-  return (
-    <>
-      <Modal config={config} />
-    </>
-  );
+  const config = {
+    name: 'Chimoney app',
+    store_img:
+      'https://lh3.googleusercontent.com/-crMj-_7sKco/AAAAAAAAAAI/AAAAAAAAAAA/8wRiFKrmpe8/s88-p-k-no-ns-nd/photo.jpg',
+
+    paymentDescription: 'Send a payment to chimoney.io',
+    supportedCurrencies: [
+      { code: 'CELO', walletAddress: 'kdkdldldldldlldldldldlld', amount: 10 },
+      { code: 'cUSD', walletAddress: 'pppp.slskofifiifi', amount: 100 },
+    ],
+  }
+  return <Modal {...config} />
 }
 
-export default App;
+export default App
--- packages/offline-pay-with-crypto-widget/src/components/Modal/index.js
+++ packages/offline-pay-with-crypto-widget/src/components/Modal/index.js
@@ -1,175 +1,272 @@
-import * as React from 'react';
-import { useState } from 'react';
-import Select from "react-select"
-import CardHeader from '@mui/material/CardHeader';
-import Avatar from '@mui/material/Avatar';
-import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
-import "./modal.css"
-import Paper from '@mui/material/Paper';
-import QRCode from "react-qr-code"
-import { motion }  from "framer-motion";
+import * as React from 'react'
+import { useState, useEffect } from 'react'
+import CardHeader from '@mui/material/CardHeader'
+import Avatar from '@mui/material/Avatar'
+import Typography from '@mui/material/Typography'
+import { red } from '@mui/material/colors'
+import { Container, TextField, MenuItem } from '@mui/material'
+import Box from '@mui/material/Box'
+import './modal.css'
+import Paper from '@mui/material/Paper'
+import QRCode from 'react-qr-code'
+import { motion } from 'framer-motion'
+import cryptoIcon from 'base64-cryptocurrency-icons'
 
 const dropIn = {
   hidden: {
-    y: "250vh",
+    y: '250vh',
     opacity: 0,
   },
   visible: {
-    y: "0",
+    y: '0',
     opacity: 1,
     transition: {
       duration: 0.3,
-      type: "spring",
+      type: 'spring',
       damping: 25,
       stiffness: 160,
     },
   },
   exit: {
-    y: "100vh",
+    y: '100vh',
     opacity: 0,
   },
-};
+}
 
-
-
-export function Modal({ config }) {
+export function Modal({
+  name = '',
+  paymentDescription = '',
+  store_img = '',
+  supportedCurrencies = [{ code: 'CELO', walletAddress: 'm', amount: 0 }],
+}) {
   // const [expanded, setExpanded] = React.useState(false);
 
-  const [state, setState] = useState("address");
-  const [email, setEmail] = useState("");
+  const [state, setState] = useState('address')
+  const [email, setEmail] = useState('')
+  const [notValid, setNotValid] = useState(false)
+  const [selectedCrypto, setSelectedCrypto] = useState(supportedCurrencies[0])
 
-  // eslint-disable-next-line no-unused-vars
-  const { name, order_type, amount, currency, address, store_img } = config;
+  const invalid = () => {
+    if (
+      !name ||
+      !paymentDescription ||
+      !supportedCurrencies ||
+      supportedCurrencies.length < 1
+    ) {
+      return true
+    }
 
-  const options = [
-    {
-      value: "Celo Dollar (cUSD)",
-      label: <span className="label"><img src="https://res.cloudinary.com/doouwbecx/image/upload/v1635965533/7236_1_vsttxa.png" alt="" /> Celo Dollar (cUSD)</span>
-    },
-    {
-      value: "Ethereum",
-      label: <span className="label"><img src="https://res.cloudinary.com/doouwbecx/image/upload/v1635964474/Cryptocurrency_r8kedk.png" alt="" /> Ethereum</span>,
+    for (const { walletAddress, amount, code } of supportedCurrencies) {
+      if (!walletAddress || !amount || !code) {
+        return true
+      }
+    }
+  }
 
-    },
-    {
-      value: "Dodge",
-      label: <span className="label"><img src="https://res.cloudinary.com/doouwbecx/image/upload/v1635965533/Cryptocurrency2_fniyvz.png" alt="" /> Dodge</span>,
-    },
-    {
-      value: "Stellar",
-      label: <span className="label"><img src="https://res.cloudinary.com/doouwbecx/image/upload/v1635965533/Cryptocurrency4_dfew2o.png" alt="" /> Stellar</span>,
-    },
-  ];
+  useEffect(() => {
+    setNotValid(invalid())
+  }, [setNotValid])
 
-
-
+  if (notValid) {
+    return (
+      <div style={{ margin: '100px' }}>
+        ERROR: Please pass the following options
+        <pre>
+          {`
+          {
+            name,
+            paymentDescription,
+            amount,
+            currency,
+            store_img,
+            supportedCurrencies: [{ code: 'CELO', walletAddress: 'address', amount: 100 }]
+          }
+          `}
+        </pre>
+        each item in supportedCurrencies should include code, walletAddress and
+        amount
+      </div>
+    )
+  }
 
   return (
     <motion.div
-    onClick={(e) => e.stopPropagation()}
-    variants={dropIn}
-    initial="hidden"
-    animate="visible"
-    exit="exit">
-    <Container id="modal-container" sx={{ bgcolor: "white"}} maxWidth="md" >
-      <Box sx={{ bgcolor: 'transparent', height: '96vh', marginTop: '3%'}}>
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            CS
-          </Avatar>
-        }
-        action={
-         <>
-  <Typography variant="body2" color="text.secondary">TOTAL</Typography>
-  <Typography variant="body2" color="text.primary">USD 3500.00</Typography>
-
-         </>
-        }
-        title={`${ (name && name) || "Chimoney Store"}`}
-        subheader={`Order Details: ${order_type}`}
-      />
-<br/>
-<br/>
-<br/>
-<Typography variant="h6" align="center"  color="text.primary">Select crypto to pay with</Typography>
-<br/>
-<Paper align="center" elevation={0} sx={{background: 'transparent'}} >
-<Select options={options} className="select-box" />
-</Paper>
-<br/>
-<br/>
-<Typography variant="body2" align="center"  color="text.primary">To Pay</Typography>
-<br/>
-<Paper align="center" elevation={0} sx={{background: 'transparent'}} >
-<Typography display="inline" variant="h5" align="center"  color="text.primary">0.00019 cUSD =</Typography>
-<Typography display="inline" variant="h6" align="center"  color="text.primary"> USD 3500.00</Typography>
-
-</Paper>
-<br/>
-        <div className="section-component">
-        <div>
-          <span
-            className={state === "address" ? "active" : "inactive"}
-            onClick={() => setState("address")}
+      onClick={(e) => e.stopPropagation()}
+      variants={dropIn}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+    >
+      <Container id="modal-container" sx={{ bgcolor: 'white' }} maxWidth="md">
+        <Box sx={{ bgcolor: 'transparent', height: '96vh', marginTop: '3%' }}>
+          <CardHeader
+            avatar={
+              <Avatar
+                sx={{ bgcolor: red[500] }}
+                aria-label="recipe"
+                src={store_img}
+              >
+                {name?.substring(0, 2)}
+              </Avatar>
+            }
+            action={
+              <>
+                <Typography variant="body2" color="text.secondary">
+                  TOTAL
+                </Typography>
+                <Typography variant="body2" color="text.primary">
+                  {`${selectedCrypto.amount} ${selectedCrypto.code}`}
+                </Typography>
+              </>
+            }
+            title={`${name}`}
+          />
+          <Typography variant="subtitle2" align="center" color="text.primary">
+            {paymentDescription}
+          </Typography>
+          <br />
+          <br />
+          <br />
+          <Typography variant="h6" align="center" color="text.primary">
+            Select crypto to pay with
+          </Typography>
+          <br />
+          <Paper
+            align="center"
+            elevation={0}
+            sx={{ background: 'transparent' }}
           >
-            Send to address
-          </span>
-          <span
-            className={state === "agent" ? "active" : "inactive"}
-            onClick={() => setState("agent")}
+            <TextField
+              id="code"
+              value={selectedCrypto}
+              onChange={(e) => {
+                setSelectedCrypto(e.target.value)
+              }}
+              fullWidth
+              label="Pay with"
+              select
+              variant="outlined"
+            >
+              {supportedCurrencies.map((currency) => {
+                const upperCode = currency.code.toUpperCase()
+                const crypto = cryptoIcon[upperCode]
+                return (
+                  <MenuItem key={currency.code} value={currency}>
+                    <span className="label">
+                      <img src={crypto?.icon} alt="" /> {currency.code} (
+                      {crypto?.name})
+                    </span>
+                  </MenuItem>
+                )
+              })}
+            </TextField>
+          </Paper>
+          <br />
+          <br />
+          <Typography variant="body2" align="center" color="text.primary">
+            To Pay
+          </Typography>
+          <br />
+          <Paper
+            align="center"
+            elevation={0}
+            sx={{ background: 'transparent' }}
           >
-            Pay through agent
-          </span>
-        </div>
-      </div>
+            <Typography
+              display="inline"
+              variant="h5"
+              align="center"
+              color="text.primary"
+            >
+              {selectedCrypto.amount} {selectedCrypto.code} =
+            </Typography>
+            <Typography
+              display="inline"
+              variant="h6"
+              align="center"
+              color="text.primary"
+            >
+              {' '}
+              {selectedCrypto.amount} {selectedCrypto.code}
+            </Typography>
+          </Paper>
+          <br />
+          <div className="section-component">
+            <div>
+              <span
+                className={state === 'address' ? 'active' : 'inactive'}
+                onClick={() => setState('address')}
+              >
+                Send to address
+              </span>
+              <span
+                className={state === 'agent' ? 'active' : 'inactive'}
+                onClick={() => setState('agent')}
+              >
+                Pay through agent
+              </span>
+            </div>
+          </div>
 
-      {
-         state === "address" ?
-           (
-             <div className="qr-container">
-               <div className="qr-div">
-                 <QRCode value="Hello" size={100} className="qr-code" />
-               </div>
-               <div className="qr-text">
-                 <h1>Scan QR Code</h1>
-                 <p>
-                   Scan code to see request details from your mobile wallet for payment
-                   to receiving address below.
-                 </p>
-                 <div className="qr-url">
-                   <span className="address">To address:</span>
-                   <span className="code"> xcvb876567nbvccvbnm </span>
-                   <img id="qr-code" src="https:img.icons8.com/material-outlined/14/2138A8/copy.png" alt="" />
-                 </div>
-               </div>
-             </div>
-           )
-           :
-           (
-             <div className="agent-section">
-               <h1>Enter your e-mail  to receive payment details to give your agent</h1>
-               <div className="agent-section-div">
-                 <label>Email</label>
-                 <br />
-                 <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="example@gmail.com" />
-               </div>
-               <div className="agent-btn">
-                 <button>Send</button>
-               </div>
-             </div>
-           )
-       }
-       <br/>
-       <br/>
-       <br/>
-       <hr className="hr"/>
-       <Typography variant="h6" align="center"  color="text.primary">Secured by cryptography</Typography>
-      </Box>
-    </Container>
+          {state === 'address' ? (
+            <div className="qr-container">
+              <div className="qr-div">
+                <QRCode
+                  value={selectedCrypto.walletAddress}
+                  size={150}
+                  className="qr-code"
+                />
+              </div>
+              <div className="qr-text">
+                <h1>Scan QR Code</h1>
+                <p>
+                  Scan code to see request details from your mobile wallet for
+                  payment to receiving address below.
+                </p>
+                <div className="qr-url">
+                  <span className="address">To address: </span>
+                  <span className="code"> {selectedCrypto.walletAddress} </span>
+                  <img
+                    id="qr-code"
+                    src="https:img.icons8.com/material-outlined/14/2138A8/copy.png"
+                    alt=""
+                  />
+                </div>
+                <Typography variant="caption">
+                  Only send {selectedCrypto.code} to the address
+                </Typography>
+              </div>
+            </div>
+          ) : (
+            <div className="agent-section">
+              <h1>
+                Enter your e-mail to receive payment details to give your agent
+              </h1>
+              <div className="agent-section-div">
+                <TextField
+                  value={email}
+                  fullWidth
+                  label="Email"
+                  variant="outlined"
+                  type="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="example@gmail.com"
+                />
+              </div>
+              <div className="agent-btn">
+                <button>Send</button>
+              </div>
+            </div>
+          )}
+          <br />
+          <br />
+          <br />
+          <hr className="hr" />
+          <Typography variant="h6" align="center" color="text.primary">
+            Secured by cryptography
+          </Typography>
+        </Box>
+      </Container>
     </motion.div>
-  );
+  )
 }
