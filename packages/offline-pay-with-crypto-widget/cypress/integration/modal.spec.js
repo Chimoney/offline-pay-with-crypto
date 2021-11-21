a8b9d1f173024f4d3224de78a5720218df18d235
Uchi Uchibeke <uchi.uchibeke@gmail.com>
2021-11-21 18:43:31 -0500

    fix email

--- packages/offline-pay-with-crypto-widget/cypress/integration/modal.spec.js
+++ packages/offline-pay-with-crypto-widget/cypress/integration/modal.spec.js
@@ -1,42 +1,40 @@
 /* eslint-disable no-undef */
 
 describe('test for the modal component', () => {
-    beforeEach(() => {
-        cy.visit('/')
-    })
+  beforeEach(() => {
+    cy.visit('/')
+  })
 
-    it('renders modal component correctly', () => {
-        cy.get('#modal-container').should('exist')
-    })
+  it('renders modal component correctly', () => {
+    cy.get('#modal-container').should('exist')
+  })
 
-    it('renders qr code image', () => {
-        cy.get('#qr-code').should('exist')
-    });
+  it('renders qr code image', () => {
+    cy.get('#qr-code').should('exist')
+  })
 
-    it('displays payment address', () => {
-        cy.get('.code').should('exist')
-    });
+  it('displays payment address', () => {
+    cy.get('.code').should('exist')
+  })
 
-    it('can switch between payment option tabs', () => {
-        cy.get('.inactive').click();
-        cy.get('.inactive').click();
-    });
+  it('can switch between payment option tabs', () => {
+    cy.get('.inactive').click()
+    cy.get('.inactive').click()
+  })
 
-    it('can select a currency', () => {
-           /* ==== Test Created with Cypress Studio ==== */
-    });
+  it('can select a currency', () => {
+    /* ==== Test Created with Cypress Studio ==== */
+  })
 
-    it('renders footer', () => {
-        cy.findAllByText('Secured by cryptography').should('exist')
-    });
+  it('renders footer', () => {
+    cy.findAllByText('Secured by cryptography').should('exist')
+  })
 
-
-    it('can enter email and accept payment through agent', function () {
-        cy.get('.inactive').click();
-        cy.get('.agent-section-div > input').clear();
-        cy.get('.agent-section-div > input').type('test@gmail.com');
-        cy.get('.agent-section-div > input').should('have.value', 'test@gmail.com')
-        cy.get('button').click();
-
-    });
-});
+  it('can enter email and accept payment through agent', function () {
+    cy.get('.inactive').click()
+    cy.get('#agent-email').clear()
+    cy.get('#agent-email').type('test@gmail.com')
+    cy.get('#agent-email').should('have.value', 'test@gmail.com')
+    cy.get('button').click()
+  })
+})
